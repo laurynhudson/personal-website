bd128c442f240b6b91dfadf2785f373b10082017
Lauryn Hudson <laurynhudson98@gmail.com>
2021-08-07 17:47:50 -0500

    Adding navbar component and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
+import NavbarComponent from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <div className="MyName">Lauryn Hudson</div>
+      <NavbarComponent></NavbarComponent>
+      <div className="MyName">LAURYN HUDSON</div>
       <div className="Welcome">
-        <h1>Hi friend - I'm Lauryn!</h1>
+        <h1>HI THERE 👋🏾 I'M LAURYN!</h1>
+      </div>
+      <div className="Intro">
+        <p>
+          I'm a software engineer in FinTech who is curious and passionate about
+          how to use technology to make the world a better place. My mission is
+          to help tech become a more inclusive space, by helping bridge the gap
+          betweent the industry and communities who have historically been
+          underrepresented, along with helping society become more sustainable
+          through technology.{" "}
+        </p>
+        <p>🌎♻️❤️🫱🏿‍🫲🏽</p>
       </div>
       <div>
         <img
@@ -15,50 +28,48 @@ function App() {
           technologist"
         ></img>
       </div>
-      <div className="Intro">
-        <h5>
-          I am a software engineer in FinTech who is curious and passionate
-          about how to use technology to make the world a better place. My
-          mission is to help tech become a more inclusive space, by helping
-          bridge the gap betweent the industry and communities who have
-          historically been underrepresented, along with helping society become
-          more sustainable through technology.{" "}
-        </h5>
-      </div>
       <div className="Experience">
-        <h2>Software Engineering</h2>
+        <h2>Software Engineering 💻</h2>
         <div className="ExperienceDetails">
-          As a software engineer at JP Morgan Chase, I...
+          As a software engineer at JP Morgan Chase, I develop, test, and deploy
+          application features with 50,000 end users. This helps streamline
+          investment bankers process for creating pitch decks.
         </div>
-      </div>
-      <div className="CommunityWork">
-        <h2>Community Work</h2>
-        <h4>Code for the Culture</h4>
-        <h4>Houston Sustainability Org</h4>
-      </div>
-      <div className="Technical Skills">
-        <h2>Stack</h2>
-        <div>
-          <h4>I currently use:</h4>
-          <div>C#/.NET, JavaScript, React, Mongo</div>
-          <div>Agile, Jira, Confluence, BitBucket, DevOps, Jules, CI/CD</div>
+        <div className="CommunityWork">
+          <h2>Community Work 🤝🏽</h2>
+          <h4>Code for the Culture</h4>
+          <h4>Houston Sustainability Org</h4>
+          <p>
+            As the lead communications officer for the Houston Sustainability
+            Workstream, I work to coordinate, plan, and host events with various
+            guest speakers to increase employees’ awareness and engagement in
+            sustainability practices.
+          </p>
         </div>
-        <div>
-          <h4>I have experience with:</h4>
-          <div>Java, Python, SQL, AWS</div>
+        <div className="Technical Skills">
+          <h2>Technologies/Frameworks ⚙️</h2>
+          <div>
+            <h4>I currently use:</h4>
+            <div>C#/.NET, JavaScript, React, Mongo</div>
+            <div>Agile, Jira, Confluence, BitBucket, DevOps, Jules, CI/CD</div>
+          </div>
+          <div>
+            <h4>I have experience with:</h4>
+            <div>Java, Python, SQL, AWS</div>
+          </div>
         </div>
-      </div>
-      <div className="Education">
-        <h2>Education</h2>
-        <div>Computer Science BS,</div>
-        <div>Xavier University of Louisiana</div>
-        <div>2016-2020</div>
-      </div>
-      <div className="ContactInfo">
-        <h2>Connect With Me</h2>
-        <div>Email: laurynhudson98@gmail.com</div>
-        <div>LinkedIn: https://www.linkedin.com/in/lauryn-hudson/</div>
-        <div>Twitter: ___lauryn</div>
+        <div className="Education">
+          <h2>Education 🎓</h2>
+          <div>Computer Science BS,</div>
+          <div>Xavier University of Louisiana</div>
+          <div>2016-2020</div>
+        </div>
+        <div className="ContactInfo">
+          <h2>Connect With Me 📲</h2>
+          <div>Email: laurynhudson98@gmail.com</div>
+          <div>LinkedIn: https://www.linkedin.com/in/lauryn-hudson/</div>
+          <div>Twitter: ___lauryn</div>
+        </div>
       </div>
     </div>
   );
